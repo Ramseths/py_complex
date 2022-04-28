89463b0dbf9b7136f5b845aa4d80c843484bc33d
Ramseths <juan.ramseths@gmail.com>
2022-04-28 18:41:24 -0500

    feat: create class Coordinate

--- scripts/myclass.py
+++ scripts/myclass.py
@@ -1,9 +1,15 @@
-from inspect import _void
-from json.tool import main
+class Coordinate:
 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, another_coordinate):
+        x_diff = (self.x -another_coordinate.x) ** 2
+        y_diff = (self.y - another_coordinate.y) ** 2
+        return (x_diff + y_diff) ** 0.5
 
-class Person:
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+if __name__ == '__main__':
+    coordinate_one = Coordinate(4, 40)
+    coordinate_two = Coordinate(5, 9)
+    print(coordinate_one.distance(coordinate_two)) 
