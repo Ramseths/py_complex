8da1ddd6b620a5a92fcf1f89cc607af3f9d6f0a2
Ramseths <juan.ramseths@gmail.com>
2022-04-30 18:29:23 -0500

    feat: añade búsqueda binaria

--- scripts/search-and-sort/linear.py
+++ scripts/search-and-sort/linear.py
@@ -1,25 +1,25 @@
 import random
 
-def linear_search(input_list, target):
+def linear_search(input_list, target, iterator = 0):
     
     # Indicador
     match = False
     
     for element in input_list:
+        iterator += 1
         if element == target:
             match = True
             break
 
-    return match
+    return [match, iterator]
 
 def main():
     list_length = int(input('De qué tamaño será la lista?'))
     target = int(input('Ingresa lo que deseas buscar en las lista'))
     input_list = [random.randint(0, 100) for i in range(list_length)]
-    find = linear_search(input_list, target)
+    find, total = linear_search(input_list, target)
     print(input_list)
-    print(f'El elemento {target} {"se encontró" if find else "no se encontró"} en la lista')
+    print(f'El elemento {target} {"se encontró" if find else "no se encontró"} en la lista con {total} iteraciones')
     
-
-if __name__ == '__ main__':
+if __name__ == '__main__':
    main()
