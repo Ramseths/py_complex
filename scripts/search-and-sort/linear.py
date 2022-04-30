41fcde654fd89889cf0db3a591d506b9efe1ff86
Ramseths <juan.ramseths@gmail.com>
2022-04-29 20:41:48 -0500

    feat: Añade búsqueda lineal

---
+++ scripts/search-and-sort/linear.py
@@ -0,0 +1,24 @@
+import random
+
+def linear_search(input_list, target):
+    
+    match = False
+    
+    for element in input_list:
+        if element == target:
+            match = True
+            break
+
+    return match
+
+def main():
+    list_length = int(input('De qué tamaño será la lista?'))
+    target = int(input('Ingresa lo que deseas buscar en las lista'))
+    input_list = [random.randint(0, 100) for i in range(list_length)]
+    find = linear_search(input_list, target)
+    print(input_list)
+    print(f'El elemento {target} {"se encontró" if find else "no se encontró"} en la lista')
+    
+
+if __name__ == '__ main__':
+   main()
